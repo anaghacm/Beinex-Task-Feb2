b07ba5f1d8ba1b7305e0847d42465dea266a4a23
anaghacm <anagharsnair@gmail.com>
2023-02-02 13:34:38 +0530

    Beinex-Task-Feb2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { TopicsComponent } from './topics/topics.component';
+import { HtmlComponent } from './html/html.component';
+import { CssComponent } from './css/css.component';
+import { BootstrapComponent } from './bootstrap/bootstrap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    TopicsComponent,
+    HtmlComponent,
+    CssComponent,
+    BootstrapComponent
   ],
   imports: [
     BrowserModule,
